825010cd97cd3e81f158b298a53598606984156f
Z572 <873216071@qq.com>
2023-03-10 20:20:52 +0800

    gnu: gamin: fix riscv64 cross-compile.
    * gnu/packages/gnome.scm: (gamin): fix riscv64 cross-compile.
    [arguments]: <#phases>: when target-riscv64? add replace-config.sub.
    [native-inputs]: when target-riscv64? add config.

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -12252,7 +12252,7 @@ repository and commit your work.")
      `(#:phases
        (modify-phases %standard-phases
          ;; The 'config.sub' is too old to recognise aarch64.
-         ,@(if (and=> (%current-target-system) target-aarch64?)
+         ,@(if (or (target-aarch64?) (target-riscv64?))
                `((add-after 'unpack 'replace-config.sub
                    (lambda _
                      (delete-file "config.sub")
@@ -12294,7 +12294,7 @@ repository and commit your work.")
      (list glib))
     (native-inputs
      `(("pkg-config" ,pkg-config)
-       ,@(if (and=> (%current-target-system) target-aarch64?)
+       ,@(if (or (target-aarch64?) (target-riscv64?))
              `(("config" ,config))
              '())))
     (home-page "https://people.gnome.org/~veillard/gamin/")
