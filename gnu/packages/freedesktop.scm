51aeda2c8ab16b59432feead3df208e7ef1a1c7b
Bruno Victal <mirai@makinata.eu>
2023-10-09 21:06:48 +0100

    gnu: Remove unneeded libxml2 from native-inputs. (libxslt)
    XML_CATALOG_FILES is already set within libxslt so it's unnecessary to include
    libxml2 for the same effect.
    
    * gnu/packages/freedesktop.scm (wayland): Remove obsolete comment.
    (farstream, elogind, packagekit, accountsservice, plymouth)[native-inputs]:
    Remove libxml2.
    * gnu/packages/benchmark.scm (sysbench)[native-inputs]: Ditto.
    * gnu/packages/cluster.scm (drbd-utils)[native-inputs]: Ditto.
    * gnu/packages/glib.scm (dbus)[native-inputs]: Ditto.
    * gnu/packages/gnome.scm (gnome-keyring, glade3, dconf, libsecret, colord)
    (upower, gnome-settings-daemon)[native-inputs]: Ditto.
    * gnu/packages/linux.scm (eudev)[native-inputs]: Ditto.
    * gnu/packages/networking.scm (iputils)[native-inputs]: Ditto.
    * gnu/packages/ocr.scm (tesseract-ocr)[native-inputs]: Ditto.
    * gnu/packages/polkit.scm (polkit)[native-inputs]: Ditto.
    * gnu/packages/samba.scm (samba/pinned)[native-inputs]: Ditto.
    * gnu/packages/sssd.scm (adcli)[native-inputs]: Ditto.
    * gnu/packages/terminals.scm (kmscon)[native-inputs]: Ditto.
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Change-Id: Ica3fb5a8e23ad928c53ae508c02777d78c635af0

--- gnu/packages/freedesktop.scm
+++ gnu/packages/freedesktop.scm
@@ -290,7 +290,6 @@ application-centers for distributions.")
            gobject-introspection
            gtk-doc/stable
            libtool
-           libxml2                      ;for XML_CATALOG_FILES
            libxslt
            perl
            pkg-config
@@ -896,7 +895,6 @@ the freedesktop.org XDG Base Directory specification.")
            docbook-xsl
            gettext-minimal
            gperf
-           libxml2                      ;for XML_CATALOG_FILES
            m4
            pkg-config
            python
@@ -1127,7 +1125,6 @@ with localed.  This package is extracted from the broader systemd package.")
            gettext-minimal
            `(,glib "bin")
            gobject-introspection
-           libxml2                      ;for XML_CATALOG_FILES
            libxslt
            pkg-config
            python-wrapper
@@ -1352,7 +1349,7 @@ Python.")
           (list pkg-config-for-build
                 this-package)           ;for wayland-scanner
           '())))
-    (inputs (list expat libxml2))       ;for XML_CATALOG_FILES
+    (inputs (list expat libxml2))
     (propagated-inputs (list libffi))
     (home-page "https://wayland.freedesktop.org/")
     (synopsis "Core Wayland window system code and protocol")
@@ -1861,7 +1858,6 @@ message bus.")
            glibc-locales                    ;for tests
            gobject-introspection
            gtk-doc
-           libxml2                      ;for XML_CATALOG_FILES
            libxslt
            pkg-config
            vala
@@ -2727,7 +2723,6 @@ Its features include:
     (native-inputs
      (list gettext-minimal
            pkg-config
-           libxml2                      ;for XML_CATALOG_FILES
            libxslt
            docbook-xsl
            docbook-xml))
