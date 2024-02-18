b7a072b57939d40d3425ff6ef49b6fbc7218516c
Josselin Poiret <dev@jpoiret.xyz>
2024-02-18 12:04:20 +0100

    gnu: accountsservice: Add libxcrypt dependency.
    * gnu/packages/freedesktop.scm (accountsservice): Add libxcrypt dependency.
    
    Change-Id: Ibafb4b79b93a15f51f8b8e5b52cb46bd429116f7

--- gnu/packages/freedesktop.scm
+++ gnu/packages/freedesktop.scm
@@ -1870,6 +1870,7 @@ message bus.")
            coreutils-minimal
            dbus
            elogind
+           libxcrypt
            shadow))
     (propagated-inputs
      ;; accountsservice.pc 'Requires' these:
