e0fcb880aecb067e70ebfebf2bbdff22989da46f
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-03-08 22:45:11 +0800

    gnu: Add syndication-6.
    * gnu/packages/kde-frameworks.scm (syndication-6): New variable.
    (syndication): Inherit above.
    
    Change-Id: Ic1c97ced5ce2083ba09212787bed59295531ed81

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -2940,8 +2940,35 @@ gallons).")
     (inputs
      (list ki18n qtbase-5))))
 
+(define-public syndication-6
+  (package
+    (name "syndication")
+    (version "6.0.0")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "mirror://kde/stable/frameworks/"
+                    (version-major+minor version) "/"
+                    name "-" version ".tar.xz"))
+              (sha256
+               (base32
+                "1bab57kvhj15dhqj35960y6dx582q9qa6vl8i49gpxpqzj4m2ppz"))))
+    (build-system cmake-build-system)
+    (native-inputs
+     (list extra-cmake-modules))
+    (inputs
+     (list kcodecs-6 qtbase))
+    (home-page "https://community.kde.org/Frameworks")
+    (synopsis "RSS/Atom parser library")
+    (description "@code{syndication} supports RSS (0.9/1.0, 0.91..2.0) and
+Atom (0.3 and 1.0) feeds.  The library offers a unified, format-agnostic view
+on the parsed feed, so that the using application does not need to distinguish
+between feed formats.")
+    (license license:lgpl2.1+)))
+
 (define-public syndication
   (package
+    (inherit syndication-6)
     (name "syndication")
     (version "5.114.0")
     (source (origin
@@ -2953,19 +2980,10 @@ gallons).")
               (sha256
                (base32
                 "13rjb1zm9yd8vbm9h7avqih5v0rr2srqwglm29l7mcnankqlh4n7"))))
-    (build-system cmake-build-system)
     (native-inputs
      (list extra-cmake-modules))
     (inputs
-     (list kcodecs qtbase-5))
-    (home-page "https://community.kde.org/Frameworks")
-    (synopsis "RSS/Atom parser library")
-    (description "@code{syndication} supports RSS (0.9/1.0, 0.91..2.0) and
-Atom (0.3 and 1.0) feeds.  The library offers a unified, format-agnostic view
-on the parsed feed, so that the using application does not need to distinguish
-between feed formats.")
-    (license license:lgpl2.1+)))
-
+     (list kcodecs qtbase-5))))
 
 ;; Tier 3
 ;;
