68f3d0c191cb5404d71bd7b5060c0a6881f46848
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-03-08 18:59:24 +0800

    gnu: Add networkmanager-qt-6.
    * gnu/packages/kde-frameworks.scm (networkmanager-qt-6): New variable.
    (networkmanager-qt): Inherit above.
    
    Change-Id: I7e080b21bae5cbdcbe5dc8ae0e967c32f519123a

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -1737,10 +1737,10 @@ information available for your modem devices, like signal, location and
 messages.")
     (license license:lgpl2.1+)))
 
-(define-public networkmanager-qt
+(define-public networkmanager-qt-6
   (package
     (name "networkmanager-qt")
-    (version "5.114.0")
+    (version "6.0.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -1749,16 +1749,16 @@ messages.")
                     name "-" version ".tar.xz"))
               (sha256
                (base32
-                "10anjsnrzawrfjlznjvvl2sbxrajl2ddnq2kgl314b5dk7z3yk4n"))))
+                "005pmcfp12swf7sj42w5rk90lbs5qzknp99crs3ndl0i9vscjpr2"))))
     (build-system cmake-build-system)
     (native-inputs
      (list extra-cmake-modules dbus pkg-config))
+    (inputs (list qtbase))
     (propagated-inputs
      ;; Headers contain #include <NetworkManager.h> and
      ;;                 #include <libnm/NetworkManager.h>
-     (list network-manager))
-    (inputs
-     (list qtbase-5))
+     (list network-manager
+           qtdeclarative))
     (arguments
      (list #:phases #~(modify-phases %standard-phases
                         (replace 'check
@@ -1773,6 +1773,29 @@ features exposed on DBus.  It allows you to manage your connections and control
 your network devices and also provides a library for parsing connection settings
 which are used in DBus communication.")
     (license license:lgpl2.1+)))
+
+(define-public networkmanager-qt
+  (package
+    (inherit networkmanager-qt-6)
+    (name "networkmanager-qt")
+    (version "5.114.0")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "mirror://kde/stable/frameworks/"
+                    (version-major+minor version) "/"
+                    name "-" version ".tar.xz"))
+              (sha256
+               (base32
+                "10anjsnrzawrfjlznjvvl2sbxrajl2ddnq2kgl314b5dk7z3yk4n"))))
+    (native-inputs
+     (list extra-cmake-modules dbus pkg-config))
+    (propagated-inputs
+     ;; Headers contain #include <NetworkManager.h> and
+     ;;                 #include <libnm/NetworkManager.h>
+     (list network-manager))
+    (inputs
+     (list qtbase-5))))
 
 (define-public oxygen-icons
   (package
