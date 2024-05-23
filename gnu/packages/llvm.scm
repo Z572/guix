5afac45bec358849b3ceebfe374be3318af7277e
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-05-23 16:08:38 +0800

    gnu: llvm: Support loongarch64.
    * gnu/packages/llvm.scm (system->llvm-target-arch): Add loongarch64 case.
    
    Change-Id: I5378063130df4194048638d352b93674a30d47a9

--- gnu/packages/llvm.scm
+++ gnu/packages/llvm.scm
@@ -128,7 +128,8 @@ as \"x86_64-linux\"."
              ("x86_64"      => "X86_64")
              ("i686"        => "X86")
              ("i586"        => "X86")
-             ("avr"         => "AVR"))))
+             ("avr"         => "AVR")
+             ("loongarch64" => "loongarch"))))
 
 (define (llvm-uri component version)
   ;; LLVM release candidate file names are formatted 'tool-A.B.C-rcN/tool-A.B.CrcN.src.tar.xz'
