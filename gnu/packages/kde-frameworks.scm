25dda1fec0584bf8affc0a4caa71a337942d5917
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-07-06 00:01:22 +0800

    gnu: kplotting: Update to 6.3.0.
    * gnu/packages/kde-frameworks.scm (kplotting): Update to 6.3.0.
    [arguments]: Set #:qtbase to qtbase.
    [inputs]: Remove qtbase-5.
    [native-inputs]: Remove qttools-5; add qttools.
    
    Change-Id: I23f74f78bfce980164cefcf57eafe7e767e51968

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -1036,7 +1036,7 @@ to flat and hierarchical lists.")
 (define-public kplotting
   (package
     (name "kplotting")
-    (version "5.114.0")
+    (version "6.3.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -1045,12 +1045,11 @@ to flat and hierarchical lists.")
                     name "-" version ".tar.xz"))
               (sha256
                (base32
-                "17x58pplln0plqiyhjpzdiqxngylxq5gkc5gk7b91xzm783x2k0n"))))
+                "08cmp86h7pwjsds2kdcnnab8nincnmp72irk9y9ansqfglsgmrzq"))))
     (build-system qt-build-system)
     (native-inputs
-     (list extra-cmake-modules qttools-5))
-    (inputs
-     (list qtbase-5))
+     (list extra-cmake-modules qttools))
+    (arguments (list #:qtbase qtbase))
     (home-page "https://community.kde.org/Frameworks")
     (synopsis "Data plotting library")
     (description "KPlotWidget is a QWidget-derived class that provides a virtual
