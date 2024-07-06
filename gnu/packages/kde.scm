d9f24d7498e4772eccec2debdf7721c12f180b9c
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-07-06 14:06:50 +0800

    gnu: kio-extras: Update to 24.05.2.
    * gnu/packages/kde.scm (kio-extras): Update to 24.05.2.
    [inputs]: Remove kactivities, kactivities-stats, kdsoap, qtbase-5, and
    qtsvg-5; add plasma-activities, plasma-activities-stats, kcmutils,
    kdsoap-qt6, kdsoap-ws-discovery-client, ktextwidgets, qtbase,
    qt5compat, qcoro-qt6, and qtsvg.
    [native-inputs]: Remove qttools-5; add qttools.
    
    Change-Id: Ic46cc008231f4838eacc440ebbe47394716d7880

--- gnu/packages/kde.scm
+++ gnu/packages/kde.scm
@@ -554,7 +554,7 @@ KDSoap.")
 (define-public kio-extras
   (package
     (name "kio-extras")
-    (version "23.04.3")
+    (version "24.05.2")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/release-service/"
@@ -562,7 +562,7 @@ KDSoap.")
                                   version ".tar.xz"))
               (sha256
                (base32
-                "1ygxfq62idpgf1dlic1245y5gf0fnkrpbqxd230xmxi7a35za8qd"))))
+                "0h720wcgsdx9h5vlr4flxrd3djmhwvlwkrf0yzwsf4amcb9wds8r"))))
     (build-system cmake-build-system)
     (arguments
      (list #:phases #~(modify-phases %standard-phases
@@ -572,21 +572,24 @@ KDSoap.")
                               (setenv "HOME" (getcwd))
                               (setenv "TMPDIR" (getcwd))
                               (invoke "ctest" "-E" "(thumbnailtest|testkioarchive)")))))))
-    (native-inputs (list extra-cmake-modules dbus kdoctools pkg-config qttools-5))
-    ;; TODO: libappimage, kdsoap-ws-discovery-client
+    (native-inputs (list extra-cmake-modules dbus kdoctools pkg-config qttools))
+    ;; TODO: libappimage
     (inputs (list gperf
                   imath
-                  kactivities
-                  kactivities-stats
+                  plasma-activities
+                  plasma-activities-stats
                   karchive
                   kbookmarks
+                  kcmutils
                   kconfig
                   kconfigwidgets
                   kcoreaddons
                   kdnssd
                   kdbusaddons
-                  kdsoap
+                  kdsoap-qt6
+                  kdsoap-ws-discovery-client
                   kguiaddons
+                  ktextwidgets
                   ki18n
                   kio
                   ksyntaxhighlighting
@@ -598,8 +601,10 @@ KDSoap.")
                   libtirpc
                   openexr
                   phonon
-                  qtbase-5
-                  qtsvg-5
+                  qtbase
+                  qt5compat
+                  qcoro-qt6
+                  qtsvg
                   samba
                   shared-mime-info
                   solid
