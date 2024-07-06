9b23b49ac3a7b5c51a19a580eb897dc52efd337d
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-07-06 20:24:19 +0800

    gnu: ktextaddons: Update to 1.5.4.
    * gnu/packages/kde-frameworks.scm (ktextaddons): Update to 1.5.4.
    [inputs]: Remove qtbase-5 and qtkeychain; add qtkeychain-qt6.
    [native-inputs]: Remove qttools-5; add qttools.
    [arguments]: Set #:qtbase to qtbase;
    <#:configure-flags>: Pass -DQT_MAJOR_VERSION=6.
    
    Change-Id: I022fd3c0fb53db4371d589a6db9108be920791fe

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -3607,7 +3607,7 @@ need.")
 (define-public ktextaddons
   (package
     (name "ktextaddons")
-    (version "1.5.3")
+    (version "1.5.4")
     (source
      (origin
        (method url-fetch)
@@ -3615,10 +3615,12 @@ need.")
              "mirror://kde/stable/" name "/" name "-" version ".tar.xz"))
        (sha256
         (base32
-         "1p0p17dnh96zmzfb91wri7bryr90pvwb07r95n6xdad8py5dnlla"))))
+         "083v4x5f46h609g8zar6x22mp1ps85ikzbr08qbfj9abx010df34"))))
     (build-system qt-build-system)
     (arguments
-     (list #:phases
+     (list #:qtbase qtbase
+           #:configure-flags #~(list "-DQT_MAJOR_VERSION=6")
+           #:phases
            #~(modify-phases %standard-phases
                (replace 'check
                  (lambda _
@@ -3630,7 +3632,7 @@ need.")
                            "(grammalecteresultwidgettest|grammalecteconfigwidgettest||grammalecteresultjobtest|languagetoolconfigwidgettest|translator-translatorwidgettest|translator-translatorengineloadertest)"))))))
     (native-inputs
      (list extra-cmake-modules
-           qttools-5))
+           qttools))
     (inputs
      (list karchive
            kconfigwidgets
@@ -3639,8 +3641,7 @@ need.")
            kio
            ksyntaxhighlighting
            kxmlgui
-           qtbase-5
-           qtkeychain
+           qtkeychain-qt6
            sonnet))
     (home-page "https://community.kde.org/Frameworks")
     (synopsis "Various text handling addons")
